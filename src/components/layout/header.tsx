f080184cf372947bcfba5a755cc288aa5bb5dce3
CesarArt <arturo_inter8@hotmail.com>
2025-10-16 09:46:36 -0600

    💄 Adjustment icon in header menu

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,22 +1,19 @@
 import Link from "next/link";
-import { Menu } from "lucide-react";
+import { House } from "lucide-react";
 import { cookies } from 'next/headers';
-
 import { LogOutButton } from "../auth/log-out-button";
-
 
 export async function Header() {
     const cookieStore = await cookies();
     const token = cookieStore.get("token");
 
-
     return (
-        <header className="sticky top-0 z-50 w-full px-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <header className="sticky text-foreground-secondary top-0 z-50 w-full px-2 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
             <div className="flex h-14 items-center">
                 <div className="flex mr-4 w-full justify-between">
                     <span className="flex justify-center items-center">
                         <Link href="/" className="mr-6 flex items-center space-x-2">
-                            <Menu />
+                            <House />
                         </Link>
                     </span>
                     {token &&
